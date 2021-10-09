b237b1ff248f0c1389acf026955cf4565e00a1ee
Grace Sutherland <rgs1690@gmail.com>
2021-10-09 10:30:54 -0500

    code along

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,38 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import getTodos from '../api/data/todoData';
 
 function Initialize() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
-
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  };
-
+  useEffect(() => {
+    getTodos().then(console.warn);
+  }, []);
   return (
-    <div className="App">
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          type="button"
-          id="this-button"
-          className="btn btn-info"
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          type="button"
-          id="that-button"
-          className="btn btn-primary mt-3"
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+    </>
   );
 }
 
