635039d5ae394cc1391c847c568ac6387282022d
Grace Sutherland <rgs1690@gmail.com>
2021-10-18 18:00:55 -0500

    fixed typo in index.js

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -62,7 +62,7 @@ function Initialize() {
             />
             <Routes
               todos={todos}
-              seTodos={setTodos}
+              setTodos={setTodos}
               setEditItem={setEditItem}
             />
           </>
