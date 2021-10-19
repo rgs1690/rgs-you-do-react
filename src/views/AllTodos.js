a9a8974aca1b07cff3ee8c47e4752085889f91d5
Grace Sutherland <rgs1690@gmail.com>
2021-10-19 08:43:10 -0500

    added setTodos prop to AllTodos to fix error and added false argument in getTodos update complete promise to fix error

--- src/views/AllTodos.js
+++ src/views/AllTodos.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { getAllTodos } from '../api/data/todoData';
 
-export default function AllTodos() {
-  const [todos, setTodos] = useState([]);
+export default function AllTodos({ setTodos }) {
+  const [allTodos, setAllTodos] = useState([]);
 
   useEffect(() => {
-    getAllTodos().then(setTodos);
+    getAllTodos().then(setAllTodos);
   }, []);
   //   const handleClick = (key) => {
   //     deleteTodo(key).then(setTodos);
   //   };
   return (
     <div>
-      {todos.map((todo) => (
-        <>
-          <div
-            key={todo.firebaseKey}
-            todo={todo}
-            setTodos={setTodos}
-            className="d-flex justify-content-between alert alert-light"
-            role="alert"
-          >
-            {todo.complete ? 'COMPLETED  ' : 'NOT COMPLETED'}
-            <div>{todo.name}</div>
-          </div>
-        </>
+      {allTodos.map((allTodo) => (
+        <div
+          key={allTodo.firebaseKey}
+          setTodos={setTodos}
+          className="d-flex justify-content-between alert alert-light"
+          role="alert"
+        >
+          {allTodo.complete ? 'COMPLETED  ' : 'NOT COMPLETED'}
+          <div>{allTodo.name}</div>
+        </div>
       ))}
     </div>
   );
 }
+AllTodos.propTypes = {
+  setTodos: PropTypes.func.isRequired,
+};
